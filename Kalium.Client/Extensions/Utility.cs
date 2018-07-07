6ad6d6cfbbdda5ab0643b7d0a45e889cc612e9e3
Chau Minh <hanhchaumn@gmail.com>
2018-07-07 13:19:58 +0700

    Create client utility methods

---
+++ Kalium.Client/Extensions/Utility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace Kalium.Client.Extensions
+{
+    public interface IUtility
+    {
+        void ShowModal(string id);
+        void HideModal();
+        void ShowLoginModal();
+        void ShowRegisterModal();
+    }
+
+    public class Utility : IUtility
+    {
+        public void ShowModal(string id)
+        {
+            HideModal();
+            RegisteredFunction.Invoke<bool>("showModal", id);
+        }
+
+        public void HideModal()
+        {
+            RegisteredFunction.Invoke<bool>("hideModal");
+        }
+
+        public void ShowLoginModal()
+        {
+            ShowModal("login-modal");
+        }
+        public void ShowRegisterModal()
+        {
+            ShowModal("login-modal");
+        }
+    }
+}
