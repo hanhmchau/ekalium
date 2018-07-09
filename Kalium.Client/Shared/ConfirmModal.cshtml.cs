e42f64b5aae3c55cace2567b6ef64550d5c0a033
Chau Minh <hanhchaumn@gmail.com>
2018-07-09 10:02:54 +0700

    Implement confirm modal

---
+++ Kalium.Client/Shared/ConfirmModal.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kalium.Client.Extensions;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace Kalium.Client.Shared
+{
+    public class ConfirmModalModel: BlazorComponent
+    {
+        [Inject]
+        protected IMegaService MegaService { get; set; }
+
+        [Parameter]
+        protected string Id { get; set; }
+        [Parameter]
+        protected string Title { get; set; }
+        [Parameter]
+        protected string Message { get; set; }
+        [Parameter]
+        protected Action YesEventHandler { get; set; }
+
+        protected void OnClickYes()
+        {
+            YesEventHandler();
+        }
+
+        protected void OnClickNo()
+        {
+            MegaService.Util.HideModal();
+        }
+    }
+}
