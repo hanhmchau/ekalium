acd55b2b369bd629f7335b6b65882fdc4a7491e3
Chau Minh <hanhchaumn@gmail.com>
2018-07-16 21:15:04 +0700

    Create admin dashboard (draft)

---
+++ Kalium.Client/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace Kalium.Client.Admin
+{
+    public class DashboardModel: BlazorComponent
+    {
+        protected override async Task OnInitAsync()
+        {
+            RegisteredFunction.Invoke<bool>("initDashboard");
+        }
+    }
+}
