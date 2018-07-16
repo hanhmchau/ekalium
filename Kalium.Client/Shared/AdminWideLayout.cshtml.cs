a532c884d4c046c0bf91d0dbe1c5b676f20cd540
Chau Minh <hanhchaumn@gmail.com>
2018-07-16 21:16:52 +0700

    Create admin wide layout

---
+++ Kalium.Client/Shared/AdminWideLayout.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kalium.Client.Extensions;
+using Kalium.Shared.Consts;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.AspNetCore.Blazor.Layouts;
+
+namespace Kalium.Client.Shared
+{
+    public class AdminWideLayoutModel: BlazorLayoutComponent
+    {
+        [Inject]
+        protected IMegaService MegaService { get; set; }
+        protected bool Loaded { get; set; }
+
+        protected override async Task OnInitAsync()
+        {
+            var user = await MegaService.AccountService.GetCurrentUser();
+            MegaService.Util.Checkpoint("/admin");
+            if (user == null)
+            {
+                MegaService.UriHelper.NavigateTo("/login");
+                return;
+            }
+            var isAuthorized = await MegaService.AccountService.IsAuthorized(Consts.Policy.ManageProducts);
+            if (!isAuthorized)
+            {
+                MegaService.UriHelper.NavigateTo("/403");
+                return;
+            }
+            RegisteredFunction.Invoke<bool>("removeCss", "outside");
+//            MegaService.Util.InitAdminComponents();
+            Console.WriteLine("I load you you load me");
+            Loaded = true;
+        }
+    }
+}
