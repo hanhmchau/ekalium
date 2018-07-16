c621acb59e20ad72dd978f22346edb8044ea3541
Chau Minh <hanhchaumn@gmail.com>
2018-07-16 21:18:52 +0700

    Create admin confirm modal

---
+++ Kalium.Client/Shared/AdminConfirmModal.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kalium.Client.Extensions;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace Kalium.Client.Shared
+{
+    public class AdminConfirmModalModel: BlazorComponent
+    {
+        [Inject]
+        protected IMegaService MegaService { get; set; }
+        [Parameter]
+        protected string Id { get; set; }
+        [Parameter]
+        protected string Title { get; set; }
+        [Parameter]
+        protected string Message { get; set; }
+        [Parameter]
+        protected Func<Task> YesEventHandler { get; set; }
+        protected async Task OnClickYes()
+        {
+            if (YesEventHandler != null)
+            {
+                await YesEventHandler?.Invoke();
+            }
+        }
+
+        protected void OnClickNo()
+        {
+            MegaService.Util.HideModal();
+        }
+    }
+}
