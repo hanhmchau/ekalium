63fcb540e32fb0b1644bc33c91a02ab0d357939f
Chau Minh <hanhchaumn@gmail.com>
2018-07-16 21:15:39 +0700

    Implement create new product

---
+++ Kalium.Client/Admin/CreateProduct.cshtml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Kalium.Client.Extensions;
+using Kalium.Shared.Models;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+using Microsoft.AspNetCore.Blazor.Components;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Kalium.Client.Admin
+{
+    public class CreateProductModel: BlazorComponent
+    {
+        [Inject]
+        protected IMegaService MegaService { get; set; }
+        protected ICollection<Category> Categories { get; set; } = new List<Category>();
+        protected ICollection<Brand> Brands { get; set; } = new List<Brand>();
+        protected ICollection<string> Origins { get; set; } = new List<string>();
+        protected ICollection<string> Materials { get; set; }
+        protected string Name { get; set; }
+        protected int Brand { get; set; }
+        protected int Category { get; set; }
+        protected string Price { get; set; }
+        protected string DiscountedPrice { get; set; }
+        protected bool HasDiscount { get; set; }
+        protected string Origin { get; set; }
+        protected string Material { get; set; }
+        protected string Features { get; set; }
+        protected string Description { get; set; }
+        protected int Quantity { get; set; }
+
+        protected override async Task OnInitAsync()
+        {
+            RegisteredFunction.Invoke<bool>("removeCss", "outside");
+            await LoadCategories();
+            await LoadAttributes();
+            RegisteredFunction.Invoke<bool>("enableCreateProductValidation", "outside");
+            MegaService.Util.InitializeSignalR();
+            MegaService.Util.InitAdminComponents();
+        }
+
+        private async Task LoadCategories()
+        {
+            JObject cateJObject = await MegaService.Fetcher.Fetch("/api/category/LoadCategories/");
+            string productJson = cateJObject["Categories"].ToString();
+            Categories = JsonConvert.DeserializeObject<ICollection<Category>>(productJson);
+        }
+
+        private async Task LoadAttributes()
+        {
+            JObject cateJObject = await MegaService.Fetcher.Fetch("/api/product/LoadBrands/");
+            string brandJson = cateJObject["Brands"].ToString();
+            Brands = JsonConvert.DeserializeObject<ICollection<Brand>>(brandJson);
+        }
+
+        protected async Task Create()
+        {
+            Brand = int.Parse(MegaService.Util.GetInput<string>("#selectize-brand").Replace("\"",""));
+            Category = int.Parse(MegaService.Util.GetInput<string>("#selectize-category").Replace("\"", ""));
+            var obj = await MegaService.Fetcher.Fetch("/api/Product/CreateProduct", new
+            {
+                Brand,
+                Category,
+                Origin,
+                Material,
+                Description,
+                Features,
+                Name,
+                Price,
+                HasDiscount,
+                DiscountedPrice
+            });
+            var url = obj["Url"].ToString();
+
+            MegaService.Util.RefreshShop();
+            MegaService.Util.ShowModal("create-success");
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            MegaService.UriHelper.NavigateTo($"/admin/product/{url}");
+        }
+    }
+}
--- Kalium.Server/Controllers/ProductController.cs
+++ Kalium.Server/Controllers/ProductController.cs
@@ -1,11 +1,18 @@
-﻿using Kalium.Server.Utils;
+﻿using System.Collections;
+using Kalium.Server.Utils;
 using Kalium.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Kalium.Server.Repositories;
+using Kalium.Shared;
 using Kalium.Shared.Consts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
 using MoreLinq;
 
 namespace Kalium.Server.Controllers
@@ -14,10 +21,12 @@ namespace Kalium.Server.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository _iProductRepository;
+        private readonly IImageRepository _imageRepository;
 
-        public ProductController(IProductRepository productRepository)
+        public ProductController(IProductRepository productRepository, IHostingEnvironment environment, IImageRepository imageRepository)
         {
             this._iProductRepository = productRepository;
+            _imageRepository = imageRepository;
         }
 
         [HttpPost("[action]")]
@@ -34,15 +43,32 @@ namespace Kalium.Server.Controllers
             var origins = parser.AsObject<ICollection<string>>("ChosenOrigins");
             var materials = parser.AsObject<ICollection<string>>("ChosenMaterials");
             var brands = parser.AsObject<ICollection<int>>("ChosenBrands");
+            var includeHidden = parser.AsBool("IncludeHidden");
 
-            var products = await _iProductRepository.SearchProducts(page, pageSize, category, minPrice, maxPrice, status, origins, materials, brands, sortType);
-            var total = await _iProductRepository.CountProducts(category, minPrice, maxPrice, status, origins, materials, brands);
+            var products = await _iProductRepository.SearchProducts(page, pageSize, category, minPrice, maxPrice, status, origins, materials, brands, sortType, includeHidden);
+            var total = await _iProductRepository.CountProducts(category, minPrice, maxPrice, status, origins, materials, brands, includeHidden);
             object result = new
             {
                 Products = products,
                 Total = total
             };
             return JsonConvert.SerializeObject(result, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+
+        [HttpPost("[action]")]
+        public async Task<string> Delete([FromBody] string json)
+        {
+            var parser = new Parser(json);
+            int id = parser.AsInt("Id");
+
+            var result = await _iProductRepository.DeleteProduct(id);
+            return JsonConvert.SerializeObject(new
+            {
+                Result = result
+            }, new JsonSerializerSettings
             {
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore
             });
@@ -69,7 +95,21 @@ namespace Kalium.Server.Controllers
         }
 
         [HttpGet("[action]")]
-        public async Task<Product> GetProductByUrl([FromQuery] string url)
+        public async Task<string> LoadBrands()
+        {
+            var brands = await _iProductRepository.GetBrands();
+            object result = new
+            {
+                Brands = brands
+            };
+            return JsonConvert.SerializeObject(result, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+
+        [HttpGet("[action]")]
+        public async Task<string> GetProductByUrl([FromQuery] string url)
         {
             var product = await _iProductRepository.FindProductByUrl(url);
             if (product != null)
@@ -81,7 +121,159 @@ namespace Kalium.Server.Controllers
                     ext.Options.ForEach(opt => opt.Extra = null); 
                 });
             }
-            return product;
+
+            var result = new
+            {
+                Product = product
+            };
+            return JsonConvert.SerializeObject(result, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                NullValueHandling = NullValueHandling.Include
+            });
+        }
+
+        [HttpGet("[action]")]
+        public async Task<string> GetProductByUrlForAdmin([FromQuery] string url)
+        {
+            var product = await _iProductRepository.FindActiveProductByUrl(url);
+            if (product != null)
+            {
+                product.Category.Products.Clear();
+                product.Brand.Products.Clear();
+                product.Coupons?.ForEach(c => { c.Product = null; });
+                product.Extras.ForEach(ext =>
+                {
+                    ext.Product = null;
+                    ext.Options.ForEach(opt => opt.Extra = null);
+                });
+            }
+            var result = new
+            {
+                Product = product
+            };
+            return JsonConvert.SerializeObject(result, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ICollection<Image>> UploadImages([FromForm] ImageViewModel model)
+        {
+            ICollection<Image> images = new List<Image>();
+            var files = model.Files;
+            if (files != null && files.Any())
+            {
+                images = _imageRepository.Save(files, Consts.Folder.Product.Name());
+                await _iProductRepository.AddImages(model.Id, images);
+            }
+
+            return images;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<string> AddCoupon([FromBody] string json)
+        {
+            var parser = new Parser(json);
+            var id = parser.AsInt("Id");
+            var coupon = parser.AsObject<Coupon>("Coupon");
+            await _iProductRepository.AddCoupon(id, coupon);
+            return JsonConvert.SerializeObject(new
+            {
+                Result = true
+            });
+        }
+
+        [HttpPost("[action]")]
+        public async Task<string> RemoveCoupon([FromBody] string json)
+        {
+            var parser = new Parser(json);
+            var id = parser.AsInt("Id");
+            var coupon = parser.AsObject<Coupon>("Coupon");
+            await _iProductRepository.RemoveCoupon(id, coupon);
+            return JsonConvert.SerializeObject(new
+            {
+                Result = true
+            });
+        }
+
+        [HttpPost("[action]")]
+        public async Task<string> RemoveExtra([FromBody] string json)
+        {
+            var parser = new Parser(json);
+            var id = parser.AsInt("Id");
+            var extra = parser.AsInt("Extra");
+            await _iProductRepository.RemoveExtra(id, extra);
+            return JsonConvert.SerializeObject(new
+            {
+                Result = true
+            });
+        }
+        [HttpPost("[action]")]
+        public async Task<string> UpdateExtra([FromBody] string json)
+        {
+            var parser = new Parser(json);
+            var id = parser.AsInt("Id");
+            var extra = parser.AsObject<Extra>("Extra");
+            var newExtra = await _iProductRepository.UpdateExtra(id, extra);
+            return JsonConvert.SerializeObject(new
+            {
+                Result = true,
+                Extra = newExtra
+            }, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+
+
+        [HttpPost("[action]")]
+        public async Task<string> RemoveImage([FromBody] string json)
+        {
+            var parser = new Parser(json);
+            var id = parser.AsInt("Id");
+            var image = parser.AsObject<Image>("Image");
+            await _iProductRepository.RemoveImage(id, image);
+            return JsonConvert.SerializeObject(new
+            {
+                Result = true
+            });
+        }
+
+        [HttpPost("[action]")]
+        public async Task<bool> Update([FromBody] Product product)
+        {
+            await _iProductRepository.Update(product);
+            return true;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<string> CreateProduct([FromBody] string json)
+        {
+            var parser = new Parser(json);
+            var name = parser.AsString("Name");
+            var nameUrl = await _iProductRepository.GenerateNameUrl(name);
+            var brand = parser.AsInt("Brand");
+            var category = parser.AsInt("Category");
+            var origin = parser.AsString("Origin");
+            var material = parser.AsString("Material");
+            var price = parser.AsDouble("Price");
+            var hasDiscount = parser.AsBool("HasDiscount");
+            var discountedPrice = hasDiscount ? parser.AsDouble("DiscountedPrice") : price;
+
+            var description = parser.AsString("Description");
+            var features = parser.AsString("Features");
+
+            var newId = await _iProductRepository.CreateProduct(name, nameUrl, brand, category,
+                origin, material, price, hasDiscount, discountedPrice, description, features);
+
+            return JsonConvert.SerializeObject(new 
+            {
+                Id = newId,
+                Url = nameUrl
+            });
         }
     }
 }
+
